30ec7b1e10a1d2cb3d803c2bbcf676760330e32c
Bjorge Meulemeester <bjorge1997@hotmail.com>
2025-05-20 01:17:04 +0200

    add pacman pkglist backup

--- init.sh
+++ init.sh
@@ -7,3 +7,16 @@ git submodule update
 
 # Stow the contents of the directory "dotfiles" to $HOME
 stow -v -R -t ~ dotfiles
+
+# Install wikiman sources: Arch and tldr
+if [[ ! -d /usr/share/doc/arch-wiki ]] || [[ ! -d /usr/share/doc/tldr-pages ]]; then
+	mkdir wikiman
+	# Download wikiman makefile
+	curl -L 'https://raw.githubusercontent.com/filiparag/wikiman/master/Makefile' -o 'wikiman/wikiman-makefile'
+	make -f ./wikiman/wikiman-makefile source-arch source-tldr
+	sudo make -f ./wikiman/wikiman-makefile source-install
+	sudo make -f ./wikiman/wikiman-makefile clean
+	rm -rf wikiman
+fi
+
+
