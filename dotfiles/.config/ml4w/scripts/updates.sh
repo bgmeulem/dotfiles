f17c0517129f363e81e3b8aef04e7c7ae1a63768
bgmeulem <bjorge1997@hotmail.com>
2025-09-23 23:21:50 +0200

    simpler update script

--- dotfiles/.config/ml4w/scripts/updates.sh
+++ dotfiles/.config/ml4w/scripts/updates.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-#  _   _           _       _             
-# | | | |_ __   __| | __ _| |_ ___  ___  
-# | | | | '_ \ / _` |/ _` | __/ _ \/ __| 
-# | |_| | |_) | (_| | (_| | ||  __/\__ \ 
-#  \___/| .__/ \__,_|\__,_|\__\___||___/ 
-#       |_|                              
-#  
-
 script_name=$(basename "$0")
-
 # Count the instances
 instance_count=$(ps aux | grep -F "$script_name" | grep -v grep | grep -v $$ | wc -l)
 
@@ -16,54 +7,26 @@ if [ $instance_count -gt 1 ]; then
     sleep $instance_count
 fi
 
-
-# ----------------------------------------------------- 
 # Define threshholds for color indicators
-# ----------------------------------------------------- 
-
 threshhold_green=0
 threshhold_yellow=25
 threshhold_red=100
-install_platform="$(cat ~/.config/ml4w/settings/platform.sh)"
 
-# Check if platform is supported
-case $install_platform in
-    arch)
-        aur_helper="$(cat ~/.config/ml4w/settings/aur.sh)"
+aur_helper="$(cat ~/.config/ml4w/settings/aur.sh)"
+check_lock_files() {
+    local pacman_lock="/var/lib/pacman/db.lck"
+    local checkup_lock="${TMPDIR:-/tmp}/checkup-db-${UID}/db.lck"
 
-        # ----------------------------------------------------- 
-        # Calculate available updates
-        # ----------------------------------------------------- 
+    while [ -f "$pacman_lock" ] || [ -f "$checkup_lock" ]; do
+        sleep 1
+    done
+}
 
-        # flatpak remote-ls --updates
+check_lock_files
 
-        # -----------------------------------------------------------------------------
-        # Check for pacman or checkupdates-with-aur database lock and wait if necessary
-        # -----------------------------------------------------------------------------
-        check_lock_files() {
-            local pacman_lock="/var/lib/pacman/db.lck"
-            local checkup_lock="${TMPDIR:-/tmp}/checkup-db-${UID}/db.lck"
+updates=$(checkupdates-with-aur | wc -l)
 
-            while [ -f "$pacman_lock" ] || [ -f "$checkup_lock" ]; do
-                sleep 1
-            done
-        }
-
-        check_lock_files
-
-        updates=$(checkupdates-with-aur | wc -l)
-    ;;
-    fedora)
-        updates=$(dnf check-update -q | grep -c ^[a-z0-9])
-    ;;
-    *)
-        updates=0
-    ;;
-esac
-
-# ----------------------------------------------------- 
 # Output in JSON format for Waybar Module custom-updates
-# ----------------------------------------------------- 
 
 css_class="green"
 
