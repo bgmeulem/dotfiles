537bf73a1f78845a7bc777b01e363baaa2e40b96
bgmeulem <bjorge1997@hotmail.com>
2025-09-29 14:43:24 +0200

    update battery script (minor)

--- dotfiles/.config/hypr/scripts/battery-ac-check.sh
+++ dotfiles/.config/hypr/scripts/battery-ac-check.sh
@@ -13,47 +13,40 @@ is_power_connected() {
     return 1  # No power connected
 }
 
-handle_power_state_change() {
-    local current_power_state
-    local previous_power_state=""
-    
-    if is_power_connected; then
-        current_power_state="connected"
-    else
-        current_power_state="disconnected"
-    fi
-    
-    # Read previous power state if file exists
-    if [ -f "$POWER_STATE_FILE" ]; then
-        previous_power_state=$(cat "$POWER_STATE_FILE")
-    fi
-    
-    # Update power state file
-    echo "$current_power_state" > "$POWER_STATE_FILE"
-    
-    # Handle power state changes
-    if [ "$previous_power_state" != "$current_power_state" ]; then
-        let "ROUNDED_BC = $BATTERY_CAPACITY / 10 * 10"
-        case "$current_power_state" in
-            "connected")
-                # Power just connected
-                if [ -f "$NOTIFICATION_ID_FILE" ]; then
-                    # Replace low battery notification
-                    NOTIFICATION_ID=$(cat "$NOTIFICATION_ID_FILE");
-                    notify-send --replace-id="$NOTIFICATION_ID" "Power Connected" "Battery charging (${BATTERY_CAPACITY}%)" -i "battery-0${ROUNDED_BC}-charging" -t 3000;
-                    rm -f "$NOTIFICATION_ID_FILE"
-                else
-                    # Show general power connected notification
-                    notify-send "Power Connected" "Battery charging (${BATTERY_CAPACITY}%)" -i "battery-0${ROUNDED_BC}-charging" -t 3000
-                fi
-                ;;
-            "disconnected")
-                # Power just disconnected
-                notify-send "Power Disconnected" "Running on battery (${BATTERY_CAPACITY}%)" -i "battery-0${ROUNDED_BC}" -t 3000
-                ;;
-        esac
-        $SOUND_DIR/sounds.sh --battery-warning
-    fi
-}
+if is_power_connected; then
+    current_power_state="connected"
+else
+    current_power_state="disconnected"
+fi
 
-handle_power_state_change
+# Read previous power state if file exists
+if [ -f "$POWER_STATE_FILE" ]; then
+    previous_power_state=$(cat "$POWER_STATE_FILE")
+fi
+
+# Update power state file
+echo "$current_power_state" > "$POWER_STATE_FILE"
+
+# Handle power state changes
+if [ "$previous_power_state" != "$current_power_state" ]; then
+    let "ROUNDED_BC = $BATTERY_CAPACITY / 10 * 10"
+    case "$current_power_state" in
+        "connected")
+            # Power just connected
+            if [ -f "$NOTIFICATION_ID_FILE" ]; then
+                # Replace low battery notification
+                NOTIFICATION_ID=$(cat "$NOTIFICATION_ID_FILE");
+                notify-send --replace-id="$NOTIFICATION_ID" "Power Connected" "Battery charging (${BATTERY_CAPACITY}%)" -i "battery-0${ROUNDED_BC}-charging" -t 3000;
+                rm -f "$NOTIFICATION_ID_FILE"
+            else
+                # Show general power connected notification
+                notify-send "Power Connected" "Battery charging (${BATTERY_CAPACITY}%)" -i "battery-0${ROUNDED_BC}-charging" -t 3000
+            fi
+            ;;
+        "disconnected")
+            # Power just disconnected
+            notify-send "Power Disconnected" "Running on battery (${BATTERY_CAPACITY}%)" -i "battery-0${ROUNDED_BC}" -t 3000
+            ;;
+    esac
+    $SOUND_DIR/sounds.sh --battery-warning
+fi
