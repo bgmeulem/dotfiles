b203ec42d151c7e01b4827995eebcf0c34865759
bgmeulem <bjorge1997@hotmail.com>
2025-10-27 18:00:42 +0100

    move cast-local-audio to separate repo

--- dotfiles/.scripts/cast/cast.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Simple wrapper script that can be aliased on unix
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-uv --project="$SCRIPT_DIR" run python "$SCRIPT_DIR"/main.py "$@";
