87a69de016a7ec65b5d5a8f3d0a674c5a2b36501
bgmeulem <bjorge1997@hotmail.com>
2025-11-05 16:34:27 +0100

    clickable screenshot notification + sound

--- dotfiles/.scripts/screenshot.sh
+++ dotfiles/.scripts/screenshot.sh
@@ -1,5 +1,10 @@
 fn=$(xdg-user-dir)/Pictures/$(date +'screenshot_%F-%T.png')
 scale=0.5
 
-grim -s "$scale" "$fn";
-notify-send -i "screengrab" "Screenshot" "$fn"
+grim -s "$scale" "$fn" && ~/.scripts/sounds.sh --screenshot
+
+user_action=$(notify-send -i "screengrab" --action "default=open" "Screenshot" "$fn");
+wait user_action
+if [[ "$user_action" == "default" ]]; then
+  nemo $fn &
+fi
