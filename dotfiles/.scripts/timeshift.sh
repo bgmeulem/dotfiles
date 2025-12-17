a5b92b6f4f58ca410efa1c700847c5d9ca38edc2
bgmeulem <bjorge1997@hotmail.com>
2025-12-18 00:18:00 +0100

    update scripts

--- dotfiles/.scripts/timeshift.sh
+++ dotfiles/.scripts/timeshift.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
+
 # Send clickable notification
-user_action=$(notify-send -i "mintbackup" --action "default=open" "Timeshift" "Click to create a backup");
-wait user_action
+user_action=$(notify-send -i "mintbackup" -u critical --action "default=open" "Timeshift" "Click to create a backup" --wait);
 if [[ "$user_action" == "default" ]]; then
   $(cat ~/.config/defaults/terminal.conf) -e sudo timeshift --create;
 fi
